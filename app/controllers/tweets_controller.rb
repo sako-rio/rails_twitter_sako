c67e995746c3e17d32ec773f72a3ca34e7c9210c
sako <sako@rio.ne.jp>
2017-08-08 11:35:01 +0900

    add serch method

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,25 @@
 class TweetsController < ApplicationController
   def index
+    client = Twitter::REST::Client.new do |config|
+      # 事前準備で取得したキーのセット
+      config.consumer_key = "Uwk2rZWiEYjhFh1h73Q0l1rQK"
+      config.consumer_secret = "IbGJid6Ww6kQoBefal0VYsHBzimjbqZabGZcOeFVP3mXkDqeGe"
+    end
+    @tweets = []
+    since_id = nil
+    # 検索ワードが存在していたらツイートを取得
+    if params[:keyword].present?
+      # リツイートを除く、検索ワードにひっかかった最新10件のツイートを取得する
+      tweets = client.search(params[:keyword], count: 10, result_type: "recent", exclude: "retweets", since_id: since_id)
+      # 取得したツイートをモデルに渡す
+      tweets.take(10).each do |tw|
+        tweet = Tweet.new(tw.full_text)
+        @tweets << tweet
+      end
+    end
+    # respond_to do |format|
+    #   format.html # show.html.erb
+    #   format.json { render json: @tweets } # jsonを指定した場合、jsonフォーマットで返す
+    # end
   end
 end
