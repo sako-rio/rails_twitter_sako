9e2ae86e65f998fc4fed50ae4140b8aab34938fe
sako <sako@rio.ne.jp>
2017-08-16 14:33:44 +0900

    Implementation Login & Logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  root to: 'tweets#index'
-
-  get 'home/index'
+  root to: 'home#index'
 
   devise_for :users, controllers: {
     omniauth_callbacks: "users/omniauth_callbacks"
   }
 
   devise_scope :user do
-    get 'sign_in', :to => 'tweets#index', :as => :new_session
+    get '/users/sign_out' => 'devise/sessions#destroy'
   end
+
+  get "tweets", to: "tweets#index", as: "user_root"
 
   get "/tweets/search" => 'tweets#index'
 end
