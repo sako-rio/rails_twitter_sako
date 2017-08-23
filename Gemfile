21bf2e1be6e356fcda6470a8f957bc8b1c9af6bb
sako <sako@rio.ne.jp>
2017-08-23 12:07:09 +0900

    add annotate master

--- Gemfile
+++ Gemfile
@@ -54,6 +54,8 @@ gem 'omniauth-google-oauth2'
 
 gem 'geocoder'
 
+gem 'annotate', require: false
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- test/models/location_test.rb
+++ test/models/location_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: locations
+#
+#  id         :integer          not null, primary key
+#  address    :string(255)
+#  latitude   :float(24)
+#  longitude  :float(24)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class LocationTest < ActiveSupport::TestCase
